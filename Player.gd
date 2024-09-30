3c98947f22f9eadccae551e16ae91f303856cd03
sesame_seed <99959941+sesameeseed@users.noreply.github.com>
2024-09-30 22:43:15 +0800

    Map and movement
    - map and player, enemy movement done according to gamestart
    
    still needs:
    - static platforms should be replaced with tiles when the sheet is done
    - ui and moving to next level
    - stop enemy from getting stuck under platform

---
+++ Player.gd
@@ -0,0 +1,57 @@
+extends CharacterBody2D
+
+
+const SPEED = 400.0
+const JUMP_VELOCITY = -400.0
+const MAX_HEALTH = 100
+const DAMAGE_INTERVAL = 2
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var health
+var timer
+var can_take_damage = true
+var sprite
+
+func _ready():
+	health = MAX_HEALTH
+	timer = $DamageTimer
+	timer.wait_time = DAMAGE_INTERVAL
+	sprite = $Sprite
+
+func _physics_process(delta):
+	# Add gravity
+	velocity.y += gravity * delta
+
+	# Handle movement
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	if Input.is_action_pressed("ui_left"):
+		velocity.x = -SPEED
+	elif Input.is_action_pressed("ui_right"):
+		velocity.x = SPEED
+	else:
+		velocity.x = lerpf(velocity.x, 0, 0.1)
+	move_and_slide()
+	
+	# Flip sprite
+	if velocity.x < 0:
+		sprite.flip_h = true
+	elif velocity.x > 0:
+		sprite.flip_h = false
+	
+	# Die if jumped off map
+	if position.y >= 1000:
+		queue_free()
+		
+func take_damage(damage_en):
+	if can_take_damage:
+		can_take_damage = false
+		timer.start()
+		health -= damage_en
+		if health <= 0:
+			queue_free()
+		print('Player lost' + str(damage_en) + 'health!')
+
+func _on_damage_timer_timeout():
+	can_take_damage = true
---
+++ enemy.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+const SPEED = 200.0
+const DAMAGE = 20.0
+
+var direction = 1
+
+func _physics_process(delta):
+	# move around 
+	velocity.x = direction * SPEED
+	var collision = move_and_collide(velocity * delta)
+	if collision:
+		direction *= -1
+		
+	# flip sprite
+	if velocity.x < 0:
+		$Sprite2D.flip_h = true
+	elif velocity.x > 0:
+		$Sprite2D.flip_h = false
+
+func _on_area_2d_body_entered(body):
+	body.take_damage(DAMAGE)
