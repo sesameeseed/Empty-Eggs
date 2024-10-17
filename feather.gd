67fe549468febe483cacecb3e9832dc4a47ec79f
sesame_seed <99959941+sesameeseed@users.noreply.github.com>
2024-10-17 21:30:55 +0800

    Added double jump, wring feather in progress
    Double jump on second up key, glide on third up key

---
+++ feather.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+const SPEED = 600.0
+
+var timer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	timer = $Timer
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
