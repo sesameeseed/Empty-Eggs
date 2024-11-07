f46034f843428f5feee608f2a93a84209f77edd6
goh-joshua <157914359+goh-joshua@users.noreply.github.com>
2024-11-07 19:02:24 +0800

    Enemy sprite

--- Moving Enemy.gd
+++ Moving Enemy.gd
@@ -28,7 +28,9 @@ func _physics_process(delta):
 	# gravity
 	if is_on_floor():
 		velocity.y = 0
+		$AnimatedSprite2D.play("default")
 	else:
+		$AnimatedSprite2D.stop()
 		velocity.y += gravity * delta
 	
 	# move around 
@@ -45,9 +47,9 @@ func _physics_process(delta):
 		
 	# flip sprite
 	if velocity.x < 0:
-		$Sprite2D.flip_h = true
+		$AnimatedSprite2D.flip_h = false
 	elif velocity.x > 0:
-		$Sprite2D.flip_h = false
+		$AnimatedSprite2D.flip_h = true
 
 # deal damage to player
 func _on_area_2d_body_entered(body):
